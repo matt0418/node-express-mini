d295da153bad650a50289ff018cf59b0c46425be
matt0418 <mfeldma7@gmail.com>
2019-02-18 16:12:36 -0500

    added ability to get users and get users by id

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // implement your API here
+const express = require('express')
+const db = require('./data/db')
+
+const server = express()
+
+server.use(express.json())
+
+
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(({message}) => {
+            res.status(500).json({
+                success: false,
+                message: { error: "The users information could not be retrieved"}
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id =req.params.id
+    db
+        .findById(id)
+        .then(user => {
+            if (user) {
+                res.status(200).json({success: true, user})
+            } else {
+                res.status(404).json({ success: false, message: {error: "The user with the specified ID does not exist"}})
+            }
+            
+        })
+        .catch(({message}) => {
+            res.status(500).json({
+                success: false,
+                message: { error: "The user informtation could not be retrieved"}
+            })
+        })
+
+})
+
+
+server.listen(4000, () => {
+    console.log(`\n*** Server Running on http://localhost:4000 ***\n`)
+})
